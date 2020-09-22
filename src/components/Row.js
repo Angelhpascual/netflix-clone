5a0a4f259c779877922c9dd1f1d68fe835f67531
angelhpascual <angelhpascual@gmail.com>
2020-09-22 22:13:49 +0200

    class isLargeRow

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,7 +4,7 @@ import "./Row.css";
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -24,8 +24,11 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row__poster"
-            src={`${baseUrl}${movie.poster_path}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
